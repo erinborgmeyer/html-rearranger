2c0157f61f8b72ce3a599ab4b71f2645d0420c1f
erinborgmeyer <borg0027@algonquinlive.com>
2016-03-16 13:20:18 -0400

    done

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
-var $belowHeading = $('<h1>');
+var $belowH1 = $('<h1>');
 var $body = $('body');
-var $belowList = $('<ul>');
+var $belowUl = $('<ul>');
 var $aboveHeading = $('<h1>');
-var $aboveList = $('<ul>');
+var $aboveUl = $('<ul>');
 
+$belowH1.html('Below ground veggies');
+
+$body.append($belowH1);
+
+$body.append($belowUl);
+
+$aboveH1.html('Above ground veggies');
+
+$body.append($aboveH1);
+
+$body.append($aboveUl);
+
+$('ul li').each(function () {
+  if ($(this).hasClass('below')) {
+    $belowUl.append($(this));
+  } else {
+    $aboveUl.append($(this));
+  }
+});
